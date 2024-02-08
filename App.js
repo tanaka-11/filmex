bf063e380d39c7f9d02276e25c92ac8399dcf585
Marina Tanaka <mtanakasg@gmail.com>
2024-02-08 09:06:13 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
-export default function App() {}
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
