6eca373ed57323854014e130bf3ff2460cc1b12f
Marina Tanaka <mtanakasg@gmail.com>
2024-02-08 10:09:37 -0300

    layout inicial

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 
       <SafeAreaView style={estilos.container}>
         <View style={estilos.viewLogo}>
-          <Text>FilmeX</Text>
+          <Text style={estilos.viewTextoLogo}>FilmeX</Text>
         </View>
 
         <View style={estilos.viewBotoes}>
@@ -32,11 +32,38 @@ export default function App() {
 }
 
 const estilos = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: "yellow",
+    flex: 1,
+  },
 
-  viewLogo: {},
+  viewLogo: {
+    backgroundColor: "green",
+    textAlign: "center",
+    flex: 5,
+    marginHorizontal: 30,
+    justifyContent: "flex-end",
+  },
 
-  viewBotoes: {},
+  viewTextoLogo: {
+    textAlign: "center",
+  },
 
-  viewRodape: {},
+  viewBotoes: {
+    flex: 3,
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    backgroundColor: "orange",
+    marginHorizontal: 30,
+  },
+
+  viewRodape: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 30,
+    backgroundColor: "red",
+  },
 });
