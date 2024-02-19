ce1440d62c2c0331499e418f8da432858bcf66bf
Marina Tanaka <mtanakasg@gmail.com>
2024-02-19 09:36:06 -0300

    criado componente para o container e adicionado no app

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import {
-  Button,
   Image,
   Pressable,
   SafeAreaView,
@@ -12,36 +11,13 @@ import {
 import logo from "./assets/images/logo.png";
 
 import { Ionicons } from "@expo/vector-icons";
-import { useFonts } from "expo-font";
-import { useCallback } from "react";
-import * as SplashScreen from "expo-splash-screen";
-
-// Configuração para manter a tela splash visível
-SplashScreen.preventAutoHideAsync();
+import SafeContainer from "./src/components/SafeContainer";
 
 export default function App() {
-  // Configuração do hook useFonts
-  const [fontsLoaded, fontError] = useFonts({
-    "Oswald-Medium": require("./assets/fonts/Oswald-Medium.ttf"), // Nome da font com o caminho
-    Outfit: require("./assets/fonts/Outfit-Regular.ttf"),
-  });
-
-  // Função hook useCallback(armazenada na memoria apenas 1 vez) de validação e carregamento da font
-  const aoAtualizarLayout = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync(); // Configuração para esconder tela splash
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
   return (
     <>
       <StatusBar barStyle="default" />
-      {/* Passado evento onLayout com a função callback das fontes */}
-      <SafeAreaView style={estilos.container} onLayout={aoAtualizarLayout}>
+      <SafeContainer>
         <View style={estilos.viewLogo}>
           <Text style={estilos.titulo}>Filme</Text>
           <Image source={logo} style={estilos.logo} />
@@ -76,19 +52,12 @@ export default function App() {
             </Text>
           </Pressable>
         </View>
-      </SafeAreaView>
+      </SafeContainer>
     </>
   );
 }
 
 const estilos = StyleSheet.create({
-  container: {
-    backgroundColor: "#000",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
   viewLogo: {
     flex: 3,
     width: "80%",
@@ -132,13 +101,12 @@ const estilos = StyleSheet.create({
 
   viewRodape: {
     width: "80%",
-    // backgroundColor: "red",
     flex: 0.5,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: "#c1071e",
     width: "100%",
+    backgroundColor: "#c1071e",
   },
 
   botaoRodape: {
--- src/components/SafeContainer.js
+++ src/components/SafeContainer.js
@@ -1,11 +1,44 @@
 // Criado com o comando "rnfs".
 import { SafeAreaView, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+// Configuração para manter a tela splash visível
+SplashScreen.preventAutoHideAsync();
 
 // Passado a props "children" para componente funcionar como um container
 export default function SafeContainer({ children }) {
-  return <SafeAreaView style={estilos.container}>{children}</SafeAreaView>;
+  // Configuração do hook useFonts
+  const [fontsLoaded, fontError] = useFonts({
+    "Oswald-Medium": require("../../assets/fonts/Oswald-Medium.ttf"), // Nome da font com o caminho
+    Outfit: require("../../assets/fonts/Outfit-Regular.ttf"),
+  });
+
+  // Função hook useCallback(armazenada na memoria apenas 1 vez) de validação e carregamento da font
+  const aoAtualizarLayout = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync(); // Configuração para esconder tela splash
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  // Passado evento onLayout com a função callback das fontes
+  return (
+    <SafeAreaView style={estilos.container} onLayout={aoAtualizarLayout}>
+      {children}
+    </SafeAreaView>
+  );
 }
 
 const estilos = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
