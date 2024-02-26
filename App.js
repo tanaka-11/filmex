df58520206a9d50634a5f84eadd9ca6fe2e52d6d
Marina Tanaka <mtanakasg@gmail.com>
2024-02-26 09:17:56 -0300

    configuração navigator com metodo stack

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import { StatusBar } from "react-native";
+import { StatusBar } from "react-native"; // Barra de Status do celular
+import { NavigationContainer } from "@react-navigation/native"; // Biblioteca de navegação
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; // Método de navegação
+
+// Telas
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
 import Privacidade from "./src/screens/Privacidade";
+
+// Programação de inicialização do método de navegação
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      {/* <Sobre /> */}
-      <Home />
-      {/* <Privacidade /> */}
+
+      {/*  Programação da biblioteca de navegação utilizando metodo Stack*/}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
