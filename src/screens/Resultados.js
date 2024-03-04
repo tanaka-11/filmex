31cd935b882a928c10dde9fea174a9e5053ef98a
Marina Tanaka <mtanakasg@gmail.com>
2024-03-04 11:57:46 -0300

    Adicionado useEffect e useState para manipular dados de busca

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SafeContainer from "../components/SafeContainer";
+import { api, apiKey } from "../services/api-moviedb";
+useEffect;
 
 // Utilizando a props nativa "route" da biblioteca de navegação. Utilizada para acessar valores passados por meio de navegação entre telas.
 export default function Resultados({ route }) {
+  // State de gerenciamento da busca na API
+  const [resultados, setResultados] = useState([]);
+
   // Capturando o parametro filme vindo da função "aoPressionarProcurar"
   const { filme } = route.params;
+
+  // useEffect com a lista de dependencias vazia para funcionamento unico.
+  useEffect(() => {
+    async function buscarFilmes() {
+      try {
+        const resposta = await api.get(`/search/movie`, {
+          params: {
+            include_adult: false,
+            language: "pt-BR",
+            query: filme,
+            api_key: apiKey,
+          },
+        }); // Endpoint com Parametros
+        console.log(resposta.data.results);
+      } catch (error) {
+        console.error("ERRO: " + error.message);
+      }
+    }
+    buscarFilmes();
+  }, []);
 
   return (
     <SafeContainer>
@@ -41,6 +67,7 @@ const estilos = StyleSheet.create({
   nomeFilme: {
     color: "#db0000",
     fontWeight: "bold",
+    textTransform: "capitalize",
   },
 
   textoRodape: {
