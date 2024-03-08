3d5557c7bb92756d8a1c9cf74d239de3f402d48b
Marina Tanaka <mtanakasg@gmail.com>
2024-03-08 11:36:49 -0300

    Update Separador.js

--- src/components/Separador.js
+++ src/components/Separador.js
@@ -1,12 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 export default function Separador() {
   return (
     <View style={estilos.viewSeparador}>
-      <AntDesign name="zhihu" size={24} color="white" />
-      <AntDesign name="zhihu" size={24} color="white" />
-      <AntDesign name="zhihu" size={24} color="white" />
+      <AntDesign name="zhihu" size={16} color="white" />
+      <AntDesign name="zhihu" size={16} color="white" />
     </View>
   );
 }
