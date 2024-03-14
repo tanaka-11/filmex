2d1bf3a9e0cc97c33fc46b1ddae6d51d1e3f75b4
Marina Tanaka <mtanakasg@gmail.com>
2024-03-14 11:26:24 -0300

    useState e UseEffect de lista de favoritos

--- src/screens/Favoritos.js
+++ src/screens/Favoritos.js
@@ -1,7 +1,39 @@
-import { StyleSheet, Text, View, Pressable, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { useEffect, useState } from "react";
 import SafeContainer from "../components/SafeContainer";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Favoritos() {
+  // State para registrar os dados carregados no storage
+  const [listaFavoritos, setlistaFavoritos] = useState([]);
+
+  // useEffect é acionado assim que entrar na tela favoritos(uma unica vez por causa da lista de dependencias vazia)
+  useEffect(() => {
+    const carregarFavoritos = async () => {
+      try {
+        // Recuperando os dados no asyncstorage
+        const dados = await AsyncStorage.getItem("@favoritosFilmex");
+
+        // Convertendo dados em objeto e os guardando no state
+        if (dados) {
+          setlistaFavoritos(JSON.parse(dados));
+        }
+      } catch (error) {
+        console.error("Erro ao carregar os dados: " + error);
+        Alert.alert("Erro", "Erro ao carregar dados.");
+      }
+    };
+    carregarFavoritos();
+  }, []);
+
+  console.log(listaFavoritos);
   return (
     <SafeContainer>
       <View style={estilos.subContainer}>
